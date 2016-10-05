e164a5f1ee1add8d3432eef01ba6a8992ac0ba10
srobertson90 <stuart.robertson.applications@gmail.com>
2016-10-05 12:07:26 +0100

    server code written

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express;
+var path = require('path');
+var Api = require('./api/api');
+
+app.use(express.static('client/build'));
+
+app.listen(3000, function(){
+  new Api(app);
+  console.log('App running on port ' + this.address().port);
+});
