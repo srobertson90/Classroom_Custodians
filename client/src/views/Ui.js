ef6af1063a22005510684e08c83cbabc9e094cd8
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-05 15:15:30 +0100

    added front end render

--- client/src/views/Ui.js
+++ client/src/views/Ui.js
@@ -3,11 +3,19 @@ var Custodians = require("../models/custodians");
 var Ui = function(){
   var custodians = new Custodians();
   custodians.all(function(data){
-    console.log(data)
-  });
+    this.render(data);
+  }.bind(this));
 }
 
 Ui.prototype = {
+  render: function(custArr){
+    var body = document.querySelector("body");
+    for(custodian of custArr){
+      var div = document.createElement("div");
+      div.innerText = custodian.name + " used " + custodian.special;
+      body.appendChild(div);
+    };
+  }
   
 }
 
